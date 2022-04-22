d0d67811f0c91bbe349e3df6db92b204c0ea72b8
lemark <marcantoine_manningham.com>
2022-04-21 22:47:45 -0400

    Amélioration de l'interface

--- src/app.rs
+++ src/app.rs
@@ -3,21 +3,30 @@ use num_cpus;
 use num_format::{Locale, ToFormattedString};
 use std::time::Instant;
 pub mod qlearning;
-use crate::app::qlearning::piles_et_action::{Paramètres, Piles};
+use crate::app::qlearning::piles_et_action::{Paramètres, Piles, NB_DE_PILE};
 
 pub struct TemplateApp {
-    label: String,
-    k: f32,
-    nb_partie: f32,
+    sortie: String,
+    sortie_piles: String,
+    k: f64,
+    nb_partie: usize,
+    nb_modèle: usize,
+    nb_coeur: usize,
+    incertitude: bool,
+    _piles: Piles,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "".to_owned(),
+            sortie: "".to_owned(),
+            sortie_piles: "".to_owned(),
             k: 0.001,
-            nb_partie: 1_000.0,
+            nb_partie: 1_000,
+            nb_modèle: 25,
+            nb_coeur: 1,
+            incertitude: false,
+            _piles: Piles([4, 3, 2, 1, 0, 0, 0, 0]),
         }
     }
 }
@@ -27,25 +36,22 @@ impl epi::App for TemplateApp {
         "Qlearning appliqué au jeu de Nim"
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
         let Self {
-            label: _,
+            sortie: _,
+            sortie_piles: _,
             k,
             nb_partie,
+            nb_modèle,
+            nb_coeur,
+            incertitude: _,
+            _piles,
         } = self;
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
+                ui.menu_button("Fichier", |ui| {
+                    if ui.button("Quitter").clicked() {
                         frame.quit();
                     }
                 });
@@ -55,24 +61,46 @@ impl epi::App for TemplateApp {
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Configuration");
 
-            ui.add(egui::Slider::new(k, 0.000001..=1.0).text("k").logarithmic(true));
+            ui.add(
+                egui::Slider::new(k, 0.000001..=1.0)
+                    .text("Vitesse de disparition de la surestimation")
+                    .logarithmic(true),
+            );
 
-            ui.add(egui::Slider::new(nb_partie, 1.0..=1_000_000.0).text("Nombre de partie").integer().logarithmic(true));
+            ui.add(
+                egui::Slider::new(nb_partie, 1..=1_000_000)
+                    .text("Nombre de partie(s)")
+                    .logarithmic(true),
+            );
+            ui.add(
+                egui::Slider::new(nb_modèle, 1..=1_000_000)
+                    .text("Nombre de modèle(s) par coeur")
+                    .logarithmic(true),
+            );
+            ui.add(egui::Slider::new(nb_coeur, 1..=num_cpus::get()).text("Nombre de coeur(s)"));
+            ui.checkbox(&mut self.incertitude, "Calcul des incertitudes");
 
+            ui.heading("Piles");
+            ui.horizontal(|ui| {
+                for index in 0..NB_DE_PILE {
+                    ui.add(
+                        egui::DragValue::new(&mut self._piles[index])
+                            .speed(0.02)
+                            .clamp_range(0..=255),
+                    );
+                }
+            });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // Milieu de
-            
             ui.heading("Résolution du jeu de Nim avec le Qlearning.");
             ui.hyperlink("https://novalemark.nohost.me/gitea/lemark/jeu_de_nim");
             if ui.button("Entrainer les modèles").clicked() {
-                let piles = Piles([4, 3, 2, 1, 0, 0, 0, 0]);
-
-                let nb_partie = self.nb_partie as usize;
-                let k = 0.01;
-                let nb_coeur = num_cpus::get();
-                let nb_modèle = 125;
+                let piles = self._piles;
+                let nb_partie = self.nb_partie;
+                let k = self.k;
+                let nb_coeur = self.nb_coeur;
+                let nb_modèle = self.nb_modèle;
 
                 let paramètres = Paramètres {
                     alpha: 0.9,
@@ -81,27 +109,44 @@ impl epi::App for TemplateApp {
                     récompense: 1.0,
                 };
 
-                self.label = format!(
-                    "Vous avez choisi des piles de {}avec un k de {} et {} modèles.\n{}",
-                    piles,
-                    k,
-                    nb_modèle * nb_coeur,
-                    self.label
-                );
-
-                if piles.xor() == 0 {
-                    self.label = format!(
-                        "Attention, le deuxième joueur devrait gagner!\n{}",
-                       self.label
-                    );
-                }
-                let avant = Instant::now();
-
                 // let nb_coup = piles.nb_coup();
-                let mut min = 100.0;
-                let mut max = 0.0;
+                let avant = Instant::now();
+                if self.incertitude {
+                    let mut min = 100.0;
+                    let mut max = 0.0;
+                    let mut somme = 0.0;
 
-                for _ in 0..10 {
+                    for _ in 0..10 {
+                        let pourcent = qlearning::teste_fiabilité(
+                            piles,
+                            nb_partie,
+                            nb_modèle,
+                            nb_coeur,
+                            paramètres,
+                        ) * 100.0;
+                        somme += pourcent;
+                        if pourcent > max {
+                            max = pourcent;
+                        }
+                        if pourcent < min {
+                            min = pourcent;
+                        }
+                    }
+
+                    let chrono = avant
+                        .elapsed()
+                        .as_millis()
+                        .to_formatted_string(&Locale::fr_CA);
+
+                    let moyenne = somme / 10.0;
+                    let incertitude = (max - min) / 2.0;
+                    let nb_partie = nb_partie.to_formatted_string(&Locale::fr_CA);
+
+                    self.sortie = format!(
+                        "{:.2}±{:.2}% avec {} parties en {} ms\n{}",
+                        moyenne, incertitude, nb_partie, chrono, self.sortie
+                    );
+                } else {
                     let pourcent = qlearning::teste_fiabilité(
                         piles,
                         nb_partie,
@@ -109,28 +154,34 @@ impl epi::App for TemplateApp {
                         nb_coeur,
                         paramètres,
                     ) * 100.0;
-                    if pourcent > max {
-                        max = pourcent;
-                    } else if pourcent < min {
-                        min = pourcent;
-                    }
+
+                    let chrono = avant
+                        .elapsed()
+                        .as_millis()
+                        .to_formatted_string(&Locale::fr_CA);
+
+                    self.sortie = format!(
+                        "{:.2}% avec {} parties en {} ms\n{}",
+                        pourcent, nb_partie, chrono, self.sortie
+                    );
                 }
 
-                let chrono = avant
-                    .elapsed()
-                    .as_millis()
-                    .to_formatted_string(&Locale::fr_CA);
-
-                let milieu = (max + min) / 2.0;
-                let incertitude = (max - min) / 2.0;
-                let nb_partie = nb_partie.to_formatted_string(&Locale::fr_CA);
-
-                self.label = format!(
-                    "{:.2}±{:.2}% avec {} parties en {} ms\n{}",
-                    milieu, incertitude, nb_partie, chrono, self.label
+                self.sortie = format!(
+                    "\nVous avez choisi des piles de {}avec un k de {} et {} modèles.\n{}",
+                    piles,
+                    k,
+                    nb_modèle * nb_coeur,
+                    self.sortie
                 );
+
+                if piles.xor() == 0 {
+                    self.sortie = format!(
+                        "Attention, le deuxième joueur devrait gagner!\n{}",
+                        self.sortie
+                    );
+                }
             }
-            ui.label(self.label.to_owned());
+            ui.label(self.sortie.to_owned());
             egui::warn_if_debug_build(ui);
         });
     }
